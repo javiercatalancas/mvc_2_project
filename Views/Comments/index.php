81d065a43efc9ff5d852c4b44dfe81bd86a16d27
Javier Catalán <74968085+javiercatalancas@users.noreply.github.com>
2021-12-11 13:54:03 +0100

    11122021
    TODO:
    terminar vista detalle (comments, user, posts)
    borrar posts

--- Views/Comments/index.php
+++ Views/Comments/index.php
@@ -10,6 +10,13 @@
             <th class="text-center">Accion</th>
         </tr>
         </thead>
+
+        <script>
+            $('#myModal').on('shown.bs.modal', function () {
+                $('#myInput').trigger('focus')
+                })
+        </script>
+
         <?php
         foreach ($comments as $comment)
         {
@@ -17,9 +24,71 @@
             echo "<td>" . $comment['id'] . "</td>";
             echo "<td>" . $comment['body'] . "</td>";
            
-            echo "<td class='text-center'><a class='btn btn-info btn-xs' href='/mvc_2_project/comments/edit/" . $comment["id"] . "' ><span class='glyphicon glyphicon-edit'></span> Editar</a> <a href='/mvc_2_project/comments/delete/" . $comment["id"] . "' class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove'></span> Borrar</a></td>";
-            echo "</tr>";
+            echo "<td class='text-center'><a class='btn btn-info btn-xs' href='/mvc_2_project/comments/edit/" . $comment["id"] . "' ><span class='glyphicon glyphicon-edit'></span> Editar</a> <a href='/mvc_2_project/comments/delete/" . $comment["id"] . "' class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove'></span> Borrar</a>
+
+           
+            <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Detalles</button>
+            <div class='modal' id='myModal'>
+            <div class='modal-dialog'>
+            <div class='modal-content'>
+
+             <div class='modal-header'>
+            <h4 class='modal-title'>VISTA DETALLE</h4>
+            <button type='button' class='close' data-dismiss='modal'>&times;</button>
+            </div>
+
+        <div class='modal-body'>
+        <p>ID: ". $comment['id'] ."</p>
+        <p>Comentario: ". $comment['body'] ."</p>";
+      
+     echo" <div class='modal-footer'>
+        <button type='button' class='btn btn-danger' data-dismiss='modal'>Close</button>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+            </td>";
+    echo "</tr>";
         }
         ?>
     </table>
 </div>
+<!-- Button to Open the Modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  Open modal
+</button>
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+  
+    
+      
+        
+         
+    <!-- ESCRIBIR LOS DATOS -->
+     
